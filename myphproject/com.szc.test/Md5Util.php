03f45c278549ccfe0ad997c55715fbe63e4fac56
travsi2016 <travis2016@126.com>
2016-11-20 20:14:40 +0800

    Initial commit

---
+++ myphproject/com.szc.test/Md5Util.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/11/13
+ * Time: 22:43
+ */
+class Md5Util
+{
+    public $name;
+    function __construct($name){
+        $this->name = $name;
+    }
+
+    public  static  function encode($test_number){
+        return $test_number+1;
+    }
+
+    function  t2(){
+        return $this->b=5;
+    }
+
+    public static function Md5Test(){
+
+    }
+}
---
+++ myphproject/mytest.php
@@ -0,0 +1,13 @@
+<?php
+  header("Content-Type: text/html; charset=UTF-8");
+
+
+    require '/com.szc.test/Md5Util.php';
+    echo "111\n";
+    $a = Md5Util::encode(3);
+    echo "a=".$a."\n";
+    if(class_exists('Md5Util')){
+        $b = new Md5Util('张三');
+        echo $b ->name;
+    }
+?>
