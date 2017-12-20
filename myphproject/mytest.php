c498fe39af08e51dd35d628697b3b1bb5e538575
travissong <szc200894@sina.com>
2017-12-20 01:22:15 -0800

    update mytestFile

--- myphproject/mytest.php
+++ myphproject/mytest.php
@@ -1,13 +1,12 @@
 <?php
   header("Content-Type: text/html; charset=UTF-8");
-
-
-    require '/com.szc.test/Md5Util.php';
-    echo "111\n";
-    $a = Md5Util::encode(3);
-    echo "a=".$a."\n";
-    if(class_exists('Md5Util')){
-        $b = new Md5Util('张三');
-        echo $b ->name;
-    }
+  require './com.szc.test/Md5Util.php';
+  
+  echo "111\n";
+  $a = Md5Util::encode(3);
+  echo "a=".$a."\n";
+  if(class_exists('Md5Util')){
+      $b = new Md5Util('张三');
+      echo $b ->name;
+  }
 ?>
