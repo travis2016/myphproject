4b5b709952c921823e3e2f8d58ae12e78b609283
travissong <szc200894@sina.com>
2017-12-20 17:57:40 -0800

    commit common new file

---
+++ myphproject/common.php
@@ -0,0 +1,15 @@
+<?php
+  header("Content-Type: text/html;charset=UTF-8");
+  require './com.szc.test/Md5Util.php';
+
+  class Persons{
+    private $names; //名字a
+
+    public function __get($pro_names){
+    	
+    }
+
+
+  }
+
+?>
