641a4da7aca25dcca2c674aa3c7f5a874b7ff8b7
travissong <szc200894@sina.com>
2018-03-27 02:26:44 -0700

    add log

--- myphproject/mytest.php
+++ myphproject/mytest.php
@@ -6,6 +6,7 @@
   echo "111<br>";
   $a = Md5Util::encode(3);
   echo "a=".$a."<br>";
+  error_log("$a\n",3,'/data1/logs/test.log');
   if(class_exists('Md5Util')){
       $b = new Md5Util('张三');
       echo $b ->name."<br>";
